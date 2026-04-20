253845f49fa6dd23b8e4888d6c08fda75e8efe75
wmx <W3051030071@163.com>
2026-04-20 20:58:31 +0800

    [Commit] Search logic

--- app/src/main/java/com/zzy/travle/data/model/mapper/SpotMapper.java
+++ app/src/main/java/com/zzy/travle/data/model/mapper/SpotMapper.java
@@ -46,6 +46,10 @@ public class SpotMapper {
     public ScenicSpotVO mapScenicSpotDOToVO(ScenicSpotDo scenicSpotDo) {
         if (scenicSpotDo == null) {
             Log.d(TAG, "[x] mapScenicSpotDOToVO #44");
+
+
+
+
             return null;
         }
 
