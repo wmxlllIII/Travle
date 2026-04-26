d5cb8e465a1527329443efd328a4c9e72123cd48
wmx <W3051030071@163.com>
2026-04-26 22:13:42 +0800

    [Commit] 设置，关于

--- app/src/main/java/com/zzy/travle/ui/activity/AboutActivity.java
+++ app/src/main/java/com/zzy/travle/ui/activity/AboutActivity.java
@@ -1,26 +1,19 @@
 package com.zzy.travle.ui.activity;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.zzy.travle.R;
+import com.zzy.travle.databinding.ActivityAboutBinding;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends BaseActivity<ActivityAboutBinding> {
+
+    public static final String TAG = "AboutActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_about);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    protected int getLayoutId() {
+        return R.layout.activity_about;
+    }
+
+    @Override
+    protected void initView() {
+        mBinding.ivBack.setOnClickListener(v -> finish());
     }
 }
--- app/src/main/java/com/zzy/travle/ui/activity/SettingActivity.java
+++ app/src/main/java/com/zzy/travle/ui/activity/SettingActivity.java
@@ -1,26 +1,59 @@
 package com.zzy.travle.ui.activity;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
 
 import com.zzy.travle.R;
+import com.zzy.travle.data.manager.AccountManager;
+import com.zzy.travle.databinding.ActivitySettingBinding;
+import com.zzy.travle.util.TravelToast;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends BaseActivity<ActivitySettingBinding> {
+
+    public static final String TAG = "SettingActivity";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_setting);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+    protected int getLayoutId() {
+        return R.layout.activity_setting;
+    }
+
+    @Override
+    protected void initView() {
+        mBinding.tvLogout.setVisibility(AccountManager.getInstance().hasToken() ? View.VISIBLE : View.GONE);
+        mBinding.tvLogout.setOnClickListener(v -> {
+            handleLogout();
         });
+        mBinding.ivBack.setOnClickListener(v -> finish());
+
+        mBinding.llAccountSecurity.setOnClickListener(v -> {
+            TravelToast.showToast(this, "即将开发账号与安全");
+        });
+
+        mBinding.llPrivacy.setOnClickListener(v -> {
+            TravelToast.showToast(this, "即将开发隐私设置");
+        });
+
+        mBinding.llNotification.setOnClickListener(v -> {
+            TravelToast.showToast(this, "即将开发消息通知");
+        });
+
+        mBinding.llClearCache.setOnClickListener(v -> {
+//            clearCache();
+        });
+
+        mBinding.llAbout.setOnClickListener(v -> {
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void handleLogout() {
+        AccountManager.getInstance().logout();
+        TravelToast.showToast(this, "已退出登录");
+
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
     }
 }
